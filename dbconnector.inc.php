b8e013e5cf0a563efee1330d6c0a7edc5653fe45
Acero <55235930+Acero6370@users.noreply.github.com>
2020-03-07 21:15:01 +0100

    Initial commit

---
+++ dbconnector.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = 'localhost';
+$database = 'to-do';
+$username = 'root';
+$password = '';
+
+// mit datenbank verbinden
+$mysqli = new mysqli($host, $username, $password, $database);
+
+// fehlermeldung, falls die Verbindung fehl schlägt.
+if ($mysqli->connect_error) {
+    die('Connect Error (' . $mysqli->connect_error . ') '. $mysqli->connect_error);
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Startseite</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+  </head>
+  <body>
+
+    <div class="container">
+      <h1>Haben sie bereits einen Account?</h1>
+      <p>
+        Drücken sie "Anmelden" falls sie bereits einen Account haben. Falls nicht drücken sie "Registrieren"
+      </p>
+      <a href="anmelden.php">Anmelden</a>
+     </br>
+      <a href="erstellen.php">Registrieren</a>
+  </body>
+</html>
